abc8663f1c0c616a675b3beb80dbacc7132e1b7f
Luciano Sobral <sobral.luciano@gmail.com>
2020-05-02 15:42:23 -0400

    feat[suitability] Tabela de pesos para gerar perfil investidor criada
    Uma tabela foi criada baseada no formulário do banco paulista, além disso
    há também uma função que retorna o perfil do investidor baseada num número
    de pontos (teoricamente obtidos de uma tabela de suitability).
    
    + Adiciona tabela de suitability
    + Adiciona função que retorna o perfil do investidor

---
+++ src/utils/constants.js
@@ -0,0 +1,83 @@
+
+/**
+ * Tabela com pesos de respostas baseadas no formulário do banco paulista
+ */
+module.exports = {
+    suitability_table : [
+        {
+            b:2,
+            c:3,
+            d:4 
+        },
+        {
+            b:2,
+            c:4,
+            d:5 
+        },
+        {
+            b:1,
+            c:2,
+            d:4 
+        },
+        {
+            b:2,
+            c:4,
+        },
+        {
+            b:2,
+            c:4,
+        },
+        {
+            b:2,
+            c:3,
+            d:4 
+        },
+        {
+            b:2,
+            c:3,
+            d:4 
+        }, 
+        {
+            a:1,
+            b:2,
+            c:4 
+        },
+        {
+            a:1,
+            b:2,
+            c:4 
+        }, 
+        {
+            b:1,
+            c:2,
+            d:4 
+        },
+        {
+            b:1,
+            c:2,
+            d:4,
+            e:5 
+        },
+    ],
+    suitability_profile(points) {
+        if(points <= 14){
+            return {
+                points,
+                'profile': 'CONSERVADOR'
+            }
+        }
+
+        if(points >= 36){
+            return {
+                points,
+                'profile': 'ARROJADO'
+            }
+        }
+
+        return {
+            points,
+            'profile': 'MODERADO'
+        }
+    }
+}
+
