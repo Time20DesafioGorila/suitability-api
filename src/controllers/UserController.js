b57fd03dc8542a6ed274eae024d249bcd6d19686
demoraes <gabrielmoraes_2013@outlook.com>
2020-05-02 09:38:53 -0400

    change UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,20 +2,38 @@ const User = require("../models/User");
 
 module.exports = {
     async index(req, res) {
-        const { email } = req.body;
-        return await User.find({ email }, (err, valor) => {});
+
+       // const { email } = req.body;
+
+        if (!req.params.id) {
+
+            const user = await User.find();
+
+            return res.json({user});
+        } else {
+
+            const user = await User.findOne();
+
+            return res.send(user);
+        }
+
+
+
+        //return await User.find({ email }, (err, valor) => { });
         /*
         const users = await User.find({ email });
         console.log(users);
  */
         /* return res.json(users); */
+
+
     },
 
     async store(req, res) {
         const { name, email, phone, password } = req.body;
 
-        if (!name) throw "Nome não informado ou inválida!";
-        if (!email) throw "E-mail não informado ou inválida!";
+        if (!name) throw "Nome não informado ou inválido!";
+        if (!email) throw "E-mail não informado ou inválido!";
 
         const user = await User.create({
             name: name,
