14cb1c8b2a13a7765f7c281667bad16a0f405776
Gabriel F. Vilar <cogumm@gmail.com>
2020-05-02 21:44:44 -0300

    refactory: Investiment Controller

--- src/controllers/InvestmentController.js
+++ src/controllers/InvestmentController.js
@@ -1,26 +1,28 @@
 const Investment = require("../models/Investment");
+const User = require("../models/User");
+const Wallet = require("../models/Wallet");
 
 module.exports = {
     async index(req, res) {
-
-        if (!req.params.id) {
-
-            const user = await User.find();
-
-            return res.json({ user });
+        if (!req.params.user_id) {
+            const investments = await Investment.find();
+            return res.json({ investments });
         } else {
-
-            const user = await User.findOne();
-
-            return res.json({ user });
+            const investments = await Investment.find();
+            return res.json({ investments });
         }
-
-
     },
 
     async store(req, res) {
-        const id = req.params;
+        const { wallet_id } = req.headers;
         const { name_inv, amount, value } = req.body;
+
+        // Validando a carteira
+        const wallets = await Wallet.findById(wallet_id);
+
+        if (!wallets) {
+            return res.status(400).json({ error: "A carteita não existe." });
+        }
 
         if (!name_inv) throw "Nome do investimento não informado ou inválido!";
         if (!value) throw "Valor não informado ou inválido!";
@@ -29,7 +31,7 @@ module.exports = {
             name_inv: name_inv,
             amount: amount,
             value: value,
-            wallet: id,
+            wallet: wallet_id,
         });
 
         /* console.log(user); */
