098217617c65a00efdff3291f064c4b62489795c
Gabriel F. Vilar <cogumm@gmail.com>
2020-05-02 22:34:09 -0300

    Alterado o request para req na Session e apagado comentários

--- src/controllers/SessionContoller.js
+++ src/controllers/SessionContoller.js
@@ -1,26 +1,16 @@
 const User = require("../models/User");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports = {
     async store(req, res) {
-        const { email } = req.body;
+        const { email, password } = req.body;
 
-        // Validando se não já existe o email
-        /*let user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select("+password");
 
-        if (!user) {
-            user = await User.create({ email });
-        }*/
+        if (!user) return res.status(400).send({ error: "Usuário não existe" });
 
-        const { email, password } = request.body;
-
-        const user = await User.findOne({ email }).select('+password');
-
-        if (!user)
-            return res.status(400).send({ error: 'Usuário não existe' });
-
-        if (!await bcrypt.compare(password, user.password))
-            return res.status(400).send({ error: 'Senha invalida' });
+        if (!(await bcrypt.compare(password, user.password)))
+            return res.status(400).send({ error: "Senha invalida" });
 
         user.password = undefined;
 
