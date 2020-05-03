24115f90e00ab24822ca7ff562d95e8f5984a92e
demoraes <gabrielmoraes_2013@outlook.com>
2020-05-02 21:30:58 -0400

    alter session

--- src/controllers/SessionContoller.js
+++ src/controllers/SessionContoller.js
@@ -1,16 +1,29 @@
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     async store(req, res) {
         const { email } = req.body;
 
         // Validando se não já existe o email
-        let user = await User.findOne({ email });
+        /*let user = await User.findOne({ email });
 
         if (!user) {
             user = await User.create({ email });
-        }
+        }*/
 
-        return res.json(user);
+        const { email, password } = request.body;
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user)
+            return res.status(400).send({ error: 'Usuário não existe' });
+
+        if (!await bcrypt.compare(password, user.password))
+            return res.status(400).send({ error: 'Senha invalida' });
+
+        user.password = undefined;
+
+        return res.json({ user });
     },
 };
