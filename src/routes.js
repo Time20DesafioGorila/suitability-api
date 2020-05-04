5cda18a85f1405e94c60c5ff5930ebca8a8935eb
demoraes <gabrielmoraes_2013@outlook.com>
2020-05-04 16:56:44 -0400

    router / comentado

--- src/routes.js
+++ src/routes.js
@@ -43,12 +43,12 @@ routes.post('/suitability', SuitabilityController.store);
 /**
  * Rota "/"
  */
-routes.get("/", (req, res) => {
+/*routes.get("/", (req, res) => {
     return res.json({
         app: "Base da API do Desafio Gorila - Mega Hack 2",
         author: "Time 20",
         rep_url: "https://github.com/Time20DesafioGorila/suitability-api",
     });
-});
+});*/
 
 module.exports = routes;
